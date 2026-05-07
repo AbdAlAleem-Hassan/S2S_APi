d0c1e933ab41e3111fc9b7e7c8d49c4a5ef7722d
ABD ALALEEM ALSAYED <allahakber1998@outlook.sa>
2026-05-07 15:15:51 +0300

    ValidateTextLength

---
+++ Sehety.Shared/Validators/TextToSignRequestValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using S2S.Shared.DataTransferObjects.V1.TranslationDTOs;
+
+namespace S2S.Shared.Validators
+{
+	public class TextToSignRequestValidator : AbstractValidator<TextToSignRequest> 
+	{
+		public TextToSignRequestValidator()
+		{
+			RuleFor(x => x.Text)
+				.MaximumLength(200).WithMessage("Text cannot exceed 200 characters.");
+		}
+	}
+}
